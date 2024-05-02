be00598df9de050ec19e1da826d4c7ab57f54c07
AndresQuiVal <quirozval.andres@hotmail.com>
2024-05-02 00:30:35 -0600

    Added new endpoints

--- app/controllers/helpers/firebase_helper.js
+++ app/controllers/helpers/firebase_helper.js
@@ -1,10 +1,8 @@
 "use strict";
 
-const firebase = require('firebase');
-require('firebase/database');  // If you are using Firebase Realtime Database
-require('firebase/firestore');  // If you are using Firestore
-
-
+const firebase = require("firebase");
+require("firebase/database"); // If you are using Firebase Realtime Database
+require("firebase/firestore"); // If you are using Firestore
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,9 +14,8 @@ const firebaseConfig = {
   storageBucket: "proyecto-dsaw.appspot.com",
   messagingSenderId: "383124467139",
   appId: "1:383124467139:web:9e89b181b3b75313c22176",
-  measurementId: "G-TMBZ5HYBC2"
+  measurementId: "G-TMBZ5HYBC2",
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
@@ -30,10 +27,9 @@ const firestore = firebase.firestore();
 
 console.log("Hola MUndo");
 
-
 function writeFirebase() {
-  database.ref('Clients').set({
-    hola: 'adios'
+  database.ref("Clients").set({
+    hola: "adios",
   });
 
   return true;
@@ -42,106 +38,192 @@ function writeFirebase() {
 function setUserLoginData(userId, password) {
   console.log(userId);
   console.log(password);
-  
-  const userRef = database.ref('Clients/' + userId);
-  userRef.once('value', snapshot => {
-    if (!snapshot.exists()) {
-      userRef.set({
-        "username": userId,
-        "password": password,
-        "date_created": new Date().toISOString(),
-        "posts": [
-          {
-            "title" : "Bienvenido a The Tech State",
-            "summary" : "Este es un ejemplo de post para tu perfil",
-            "description" : "This is supposed to be a long format desc",
-            "section" : "Tech",
-            "img_url" : "https://i.ibb.co/QcyzcTh/left-image.png",
-            "comments" : [
+
+  const userRef = database.ref("Clients/" + userId);
+  return new Promise((resolve, reject) => {
+    userRef.once("value", (snapshot) => {
+      if (!snapshot.exists()) {
+        userRef
+          .set({
+            username: userId,
+            password: password,
+            date_created: new Date().toISOString(),
+            posts: [
               {
-                "username" : "AndresQuiVal",
-                "message" : "Hola, te acaba de comentar el admin! bienvenido a The Tech State!"
-              }
-            ]
-          }
-        ],
-        "comments": [] // is not added but its supposed to!
-      });
-
-    }
+                title: "Bienvenido a The Tech State",
+                summary: "Este es un ejemplo de post para tu perfil",
+                description: "This is supposed to be a long format desc",
+                section: "Tech",
+                img_url: "https://i.ibb.co/QcyzcTh/left-image.png",
+                comments: [
+                  {
+                    username: "AndresQuiVal",
+                    message:
+                      "Hola, te acaba de comentar el admin! bienvenido a The Tech State!",
+                  },
+                ],
+              },
+            ],
+            comments: [], // is not added but its supposed to!
+          })
+          .then(() => {
+            // After setting the data, resolve the Promise with false
+            resolve(false);
+          })
+          .catch((error) => {
+            // If there is any error in setting the data, reject the Promise
+            reject(error);
+          });
+      } else {
+        resolve(true);
+      }
+    });
   });
-
 }
 
+function validatePassword(username, password) {
+  return new Promise((resolve, reject) => {
+    const userRef = database.ref(`Clients/${username}/password`);
 
+    userRef
+      .once("value", (snapshot) => {
+        const passwordDB = snapshot.val();
+        if (password === passwordDB) {
+          resolve(true);
+        }
+
+        resolve(false);
+      })
+      .catch((error) => {
+        // If there is any error in setting the data, reject the Promise
+        reject(false);
+      });
+  });
+}
 
 function getUserPosts(username, token) {
   return new Promise((resolve, reject) => {
-      // Here, validate the token first (this part is just pseudocode)
-      validateToken(token).then(isValid => {
-          if (!isValid) {
-              reject("Invalid token");
-              return;
+    // Here, validate the token first (this part is just pseudocode)
+    validateToken(username, token)
+      .then((isValid) => {
+        if (!isValid) {
+          reject("Invalid token");
+          return;
+        }
+
+        const postsRef = database.ref(`Clients/${username}/posts`);
+
+        postsRef.once(
+          "value",
+          (snapshot) => {
+            if (snapshot.exists()) {
+              resolve(snapshot.val());
+            } else {
+              resolve([]); // Return an empty array if there are no posts
+            }
+          },
+          (error) => {
+            reject(error);
           }
-
-          const postsRef = database.ref(`Clients/${username}/posts`);
-
-          postsRef.once('value', snapshot => {
-              if (snapshot.exists()) {
-                  resolve(snapshot.val());
-              } else {
-                  resolve([]); // Return an empty array if there are no posts
-              }
-          }, error => {
-              reject(error);
-          });
-      }).catch(error => {
-          reject("Token validation failed");
+        );
+      })
+      .catch((error) => {
+        reject("Token validation failed");
       });
   });
 }
 
 function validateToken(username, token) {
   return new Promise((resolve, reject) => {
-      const userRef = database.ref(`Clients/${username}/userTOKEN`);
+    const userRef = database.ref(`Clients/${username}/userTOKEN`);
 
-      userRef.once('value', snapshot => {
+    userRef
+      .once("value", (snapshot) => {
         if (snapshot.exists()) {
-            const userToken = snapshot.val(); 
-            if (userToken === token) {
-                resolve(true); 
-            } else {
-                resolve(false); 
-            }
+          const userToken = snapshot.val();
+          if (userToken === token) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
         } else {
-            resolve(false); 
+          resolve(false);
         }
-      }).catch(error => {
-          reject(error);
+      })
+      .catch((error) => {
+        reject(error);
       });
   });
 }
 
+function getPostById(username, postId) {
+  return new Promise((resolve, reject) => {
+    const postRef = db.ref(`Clients/${username}/posts/${postId}`);
+    postRef
+      .once("value", (snapshot) => {
+        if (snapshot.exists()) {
+          resolve(snapshot.val()); // Returns the post data as JSON
+        } else {
+          resolve(null); // No post found at the given index
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function deletePostById(username, postId, userTOKEN) {
+  return new Promise((resolve, reject) => {
+    validateToken(username, userTOKEN).then((isValid) => {
+      if (!isValid) {
+        reject("Invalid token");
+        return;
+      }
+      const postRef = db.ref(`Clients/${userId}/posts/${postId}`);
+      postRef
+        .once("value", (snapshot) => {
+          if (snapshot.exists()) {
+            postRef.remove()
+              .then(() => resolve(true))
+              .catch(error => reject(error));
+          } else {
+            resolve(false); // No post found at the given index
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+}
 
 function setUserToken(userId, userToken) {
-  const userRef = database.ref('users/' + userId); // Adjust path according to your database structure
+  const userRef = database.ref("Clients/" + userId);
 
-  return userRef.update({
-    userTOKEN: userToken
-  })
-  .then(() => {
-    console.log('User token updated successfully.');
-    return { success: true, message: 'Token updated' };
-  })
-  .catch(error => {
-    console.error('Error updating user token:', error);
-    return { success: false, message: 'Failed to update token', error: error };
-  });
+  return userRef
+    .update({
+      userTOKEN: userToken,
+    })
+    .then(() => {
+      console.log("User token updated successfully.");
+      return { success: true, message: "Token updated" };
+    })
+    .catch((error) => {
+      console.error("Error updating user token:", error);
+      return {
+        success: false,
+        message: "Failed to update token",
+        error: error,
+      };
+    });
 }
 
 exports.writeFirebase = writeFirebase;
 exports.setUserLoginData = setUserLoginData;
+exports.validatePassword = validatePassword;
 exports.setUserToken = setUserToken;
 exports.getUserPosts = getUserPosts;
-
-
+exports.getPostById = getPostById;
+exports.validateToken = validateToken;
+exports.deletePostById = deletePostById;
--- app/controllers/router.js
+++ app/controllers/router.js
@@ -25,12 +25,21 @@ router.get('/users/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'login.html'));
 });
 
-router.post('/users/login', (req, res) => {
+router.post('/users/login', async (req, res) => {
     const userId = req.body.username;
     const password = req.body.password; // Ensure password is securely handled and stored
-    let resDB = firebaseHelper.setUserLoginData(userId, password);
+    let resDB = await firebaseHelper.setUserLoginData(userId, password);
 
     console.log(resDB);
+    if (resDB) {
+        // validate password
+        let isValidPassword = await firebaseHelper.validatePassword(userId, password);
+        if (!isValidPassword) {
+            // redirect to index()
+            return res.status(400).send('{"state" : "error", "message" : "Password incorrect" ');
+            return res.redirect('/'); // ADRIAN esta es una opcion, si es que quieres cargar desde el front
+        }
+    }
     
     // this model is defined under controllers/models, and is the 
     // base for the API responses 
@@ -39,6 +48,23 @@ router.post('/users/login', (req, res) => {
     resDB = firebaseHelper.setUserToken(userId, userTOKEN);
     // push that token into the database
     res.status(200).send('{"state" : "success", "message" : "' + userTOKEN + '" }');
+});
+
+
+
+
+// Nos sirve para validar en cada peticion si el usuario esta logueado o no!
+router.post('/users/is-logged-in', async (req, res) => {
+    const userId = req.body.username;
+    const userTOKEN = req.body.userTOKEN; // Ensure password is securely handled and stored
+    let resDB = await firebaseHelper.validateToken(userId, userTOKEN);
+
+    if (!resDB) {
+        // is not logged in, notify user
+        return res.status(400).send('{"state" : "error", "message" : "Token does not matches" ');
+    }
+
+    return res.status(200).send('{"state" : "success", "message" : "Token matches" ');
 });
 
 
@@ -58,6 +84,9 @@ router.get('/write_database', (req, res) => {
 // that field should be stores in localStorage or sessionStorage inside the browser
 // and when doing requests, use that field inside the api requests and done!
 
+
+
+// routes for post detail
 
 router.get('/my-posts/:username', (req, res) => {
     const username = req.params.username;
@@ -80,6 +109,134 @@ router.get('/my-posts/:username', (req, res) => {
 });
 
 
+
+// CRUD POSTS
+
+router.get('/users/:username/:post_id', async (req, res) => {
+    const username = req.params.username;
+    const post_id = req.params.post_id;
+
+    // now check if the post exists
+    let post_info = await firebaseHelper.getPostById(username, post_id);
+    if (post_info === undefined) {
+        // post does not exist
+        // we can send them to a 404 page
+        // ADRIAN, aqui si retorna 404, mandalo a un not found
+        return res.status(404).send('{"state" : "error", "message" : "Post not found" ');
+    }
+
+    let postDetailOnString = JSON.stringify(post_info);
+    return res.status(200).send(`{"state" : "success", "message" : "${postDetailOnString}"`);
+
+    // ADRIAN, aqui se va a retornar toda la info del post, por lo tanto es importante que
+    // cuando el usuario habra el post detail siempre se verifique que
+    // si el es el owner del post, entonces pueda modificar la info en el html, de lo contrario
+    // le aparecera en readonly, eso lo haces con el userTOKEN
+
+    // para validar si es el owner, simplemente manda a llamar al endpoint de /is-logged-in/ 
+    // donde le pasas por parametro el username del post que se acaba de abrir, mas el userTOKEN
+    // que tienes guardado en el session storage y si sale bien, es por que es su post y puede editarlo!
+
+});
+
+
+
+// TODO PENDING ANDRES!
+
+router.post('/users/:username/post/', async (req, res) => {
+    const username = req.params.username;
+    const post_id = req.params.post_id;
+
+    const token = req.headers['authorization']; // Commonly tokens are passed in the 'Authorization' header
+
+    // Check if the token is present
+    if (!token) {
+        return res.status(401).send("Access denied. No token provided.");
+    }
+
+    // now check if the post exists
+    let post_info = await firebaseHelper.getPostById(username, post_id);
+    if (post_info === undefined) {
+        // post does not exist
+        // we can send them to a 404 page
+        // ADRIAN, aqui si retorna 404, mandalo a un not found
+        return res.status(404).send('{"state" : "error", "message" : "Post not found" ');
+    }
+
+    let postDetailOnString = JSON.stringify(post_info);
+    return res.status(200).send(`{"state" : "success", "message" : "${postDetailOnString}"`);
+
+    // ADRIAN, aqui se va a retornar toda la info del post, por lo tanto es importante que
+    // cuando el usuario habra el post detail siempre se verifique que
+    // si el es el owner del post, entonces pueda modificar la info en el html, de lo contrario
+    // le aparecera en readonly, eso lo haces con el userTOKEN
+
+    // para validar si es el owner, simplemente manda a llamar al endpoint de /is-logged-in/ 
+    // donde le pasas por parametro el username del post que se acaba de abrir, mas el userTOKEN
+    // que tienes guardado en el session storage y si sale bien, es por que es su post y puede editarlo!
+
+});
+
+// TODO PENDING! ANDRES
+
+router.put('/users/username:/post_id:/', async (req, res) => {
+    const username = req.params.username;
+    const post_id = req.params.post_id;
+
+    const token = req.headers['authorization']; // Commonly tokens are passed in the 'Authorization' header
+
+    // Check if the token is present
+    if (!token) {
+        return res.status(401).send("Access denied. No token provided.");
+    }
+
+    // now check if the post exists
+    let post_info = await firebaseHelper.getPostById(username, post_id);
+    if (post_info === undefined) {
+        // post does not exist
+        // we can send them to a 404 page
+        // ADRIAN, aqui si retorna 404, mandalo a un not found
+        return res.status(404).send('{"state" : "error", "message" : "Post not found" ');
+    }
+
+    let postDetailOnString = JSON.stringify(post_info);
+    return res.status(200).send(`{"state" : "success", "message" : "${postDetailOnString}"`);
+
+    // ADRIAN, aqui se va a retornar toda la info del post, por lo tanto es importante que
+    // cuando el usuario habra el post detail siempre se verifique que
+    // si el es el owner del post, entonces pueda modificar la info en el html, de lo contrario
+    // le aparecera en readonly, eso lo haces con el userTOKEN
+
+    // para validar si es el owner, simplemente manda a llamar al endpoint de /is-logged-in/ 
+    // donde le pasas por parametro el username del post que se acaba de abrir, mas el userTOKEN
+    // que tienes guardado en el session storage y si sale bien, es por que es su post y puede editarlo!
+
+});
+
+
+router.delete('/users/username:/post_id:/', async (req, res) => {
+    const username = req.params.username;
+    const post_id = req.params.post_id;
+
+    const token = req.headers['authorization']; // Commonly tokens are passed in the 'Authorization' header
+
+    // Check if the token is present
+    if (!token) {
+        return res.status(401).send("Access denied. No token provided.");
+    }
+
+    // now check if the post exists
+    let resDB = await firebaseHelper.deletePostById(username, post_id, token);
+    if (resDB === false) {
+        // cannot delete post, probably beacuse of ownership
+        return res.status(400).send('{"state" : "error", "message" : "Could not delete post" ');
+    }
+
+    return res.status(200).send(`{"state" : "success", "message" : "Post deleted"`);
+
+    // ADRIAN, aqui se regresa al /my-posts/ del usuario logueado
+
+});
 
 
 function validateAdmin(req, res, next) {
